76900a46736f887a2b6a5e4ab601568be5a81e72
Firoz Ahmed <firozsaifi1234@gmail.com>
2020-12-22 20:40:27 +0530

    give final touch to home page

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$('.testimonial_owlCarousel').owlCarousel({
+            loop:true,
+            margin:10,
+            dots:false,
+            nav:true,
+            autoplay:false,   
+            smartSpeed: 3000, 
+            autoplayTimeout:4000,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            }
+        })
